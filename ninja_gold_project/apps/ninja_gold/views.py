5ffe5d81d3fecf979bac1d44b5a76f4d795503cb
Gerardo <jimgerar@amazon.com>
2018-04-07 14:29:50 -0600

    ninja gold completed

--- ninja_gold_project/apps/ninja_gold/views.py
+++ ninja_gold_project/apps/ninja_gold/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render, redirect
+from operator import itemgetter
+from random import randint
+import datetime
 
 # Create your views here.
 def index(request):
@@ -11,8 +13,29 @@ def index(request):
     return render (request, 'ninja_gold/index.html')
 
 def get_money(request):
+    data = {
+        'gold': 0,
+        'time': datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"),
+        'money_source': request.POST['money_source']
+    }
+    if request.POST['money_source'] == 'farm':
+        data['gold'] += randint(10,20)
+    elif request.POST['money_source'] == 'cave':
+        data['gold'] += randint(5,10)
+    elif request.POST['money_source'] == 'house':
+        data['gold'] += randint(2,5)
+    else:
+        data['gold'] += randint(-50,50)
+
+    request.session['total_gold'] += data['gold']
+
+    request.session['activities'].append(data)
+    request.session.modified = True
+
+    request.session['activities']=sorted(request.session['activities'], key=itemgetter('time'), reverse=True)
 
     return redirect ('/')
 
 def clear(request):
+    request.session.clear()
     return redirect ('/')
--- ninja_gold_project/ninja_gold_project/settings.py
+++ ninja_gold_project/ninja_gold_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mathfilters',
     'apps.ninja_gold',
     'django.contrib.admin',
     'django.contrib.auth',
